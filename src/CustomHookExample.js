0ca9179ebd9db27e17d15ebfb0d158748f6699ca
Rick Miyamoto <ricmiyam@amazon.com>
2021-04-19 16:14:27 -0700

    add custom hook example

---
+++ src/CustomHookExample.js
@@ -0,0 +1,25 @@
+import './App.css';
+import {useForm} from "./hooks/useForm";
+
+function CustomHookExample() {
+    const [form, onChangeForm] = useForm({firstName: '', lastName: ''});
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <div>
+                    <strong>First Name: {form.firstName}</strong>
+                </div>
+                <div>
+                    <strong>Last Name: {form.lastName}</strong>
+                </div>
+                <form>
+                    <input name={'firstName'} onChange={onChangeForm} placeholder={'Your first name'}></input>
+                    <input name={'lastName'} onChange={onChangeForm} placeholder={'Your last name'}></input>
+                </form>
+            </header>
+        </div>
+    );
+}
+
+export default CustomHookExample;
