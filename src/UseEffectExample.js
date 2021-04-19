58cab5101bf86a31a539c2dfac9b87b4df8afffc
Rick Miyamoto <ricmiyam@amazon.com>
2021-04-19 16:46:45 -0700

    add more examples to UseEffectExample.js

--- src/UseEffectExample.js
+++ src/UseEffectExample.js
@@ -6,14 +6,19 @@ function UseEffectExample() {
     const [form, onChangeForm] = useForm({firstName: '', lastName: ''});
 
     // When you want to do something after the component mounts. Note the `[]` as the 2nd argument.
-    // This is similar to componentDidMount and it will not be called on every re-render
+    // This is similar to componentDidMount() in Class based components and it will not be called on every re-render
 
     useEffect(() => {
         console.log('This is only called once');
+
+        // To invoke a callback function for componentDidUnmount(), return the function here
+        return () => {
+            console.log("Unmounted!")
+        }
     }, [])
 
-    // When firstName or lastName changes, print log statements accordingly
-    // This is similar to componentDidUpdate(prevState) in Class based components
+    // When firstName or lastName changes, print log statements accordingly. Note the changed value in the 2nd argument.
+    // This is similar to componentDidUpdate() in Class based components
 
     useEffect(() => {
         console.log('First name changed!');
@@ -22,6 +27,11 @@ function UseEffectExample() {
     useEffect(() => {
         console.log('Last name changed!');
     }, [form.lastName])
+
+    // The 2nd argument is a list of dependencies. If ANY value inside the list of dependencies change, the callback is triggered
+    useEffect(() => {
+        console.log('Either firstName or lastName changed');
+    }, [form.firstName, form.lastName])
 
     return (
         <div className="App">
